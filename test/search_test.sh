e1637090ba306e26fbf06c366bc6dff4b8c001e2
tikisi <faohfh@gmail.com>
2021-10-27 11:19:45 +0900

    add testing code for searching

--- test/search_test.sh
+++ test/search_test.sh
@@ -3,6 +3,8 @@
 set -x
 curl -X GET -c cookie.txt http://localhost:8000/login/ | grep csrfmiddlewaretoken
 curl -i -X POST -b cookie.txt -c cookie.txt -d 'mailAddress=fuga@test.hoge.jp&password=password&csrfmiddlewaretoken=XXXXX' http://localhost:8000/login
-#curl -i -X POST -b cookie.txt -c cookie.txt -d 'isbn=9784627705623&csrfmiddlewaretoken=XXXXX' http://localhost:8000/search
+# Existing ISBN
+curl -X GET -b cookie.txt -d 'isbn=9784627705623' http://localhost:8000/search
+# Not existing ISBN
 curl -X GET -b cookie.txt -d 'isbn=8784627705623' http://localhost:8000/search
 set +x
