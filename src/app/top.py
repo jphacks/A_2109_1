d2b68009a5c7ee9989e8a1b9e474d7dc702c84e5
koda_ryuta@plise.co.jp <s1260050@u-aizu.ac.jp>
2021-10-30 04:41:47 +0900

    一時停止

--- src/app/top.py
+++ src/app/top.py
@@ -1,15 +1,21 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 from flask_login import login_required, current_user
 from app import app
 
 bp = Blueprint('top', __name__)
 
-@bp.route('/top')
+@bp.route('/top', methods=['GET'])
 @login_required
 def top():
     with app.db.cursor() as cursor:
-        sql = "SELECT name FROM user where ID = %s;"
-        cursor.execute(sql, current_user.id)
-        result = cursor.fetchall()
-
-        return 'User Name is ' + result[0]['name']
+        #sql ='''
+        #SELECT book.ID
+        #FROM book
+        #GROUP BY book.ID DESC 
+        #LIMIT 0,10
+        #'''
+        #cursor.execute(sql)
+        #result = cursor.fetchall()
+        
+        print(result)
+        return jsonify({"message": "Successfully!!", "articles": result}), 400
