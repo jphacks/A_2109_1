35761b07f130c02e92387e9c5b1b006bcc6e3330
tikisi <faohfh@gmail.com>
2021-10-29 22:51:58 +0900

    Complete API about articles

--- src/app/article.py
+++ src/app/article.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, jsonify
 from flask_login import login_required, current_user
 from app import app
 import pymysql
+import datetime
 
 bp = Blueprint('article', __name__, url_prefix='/article')
 
@@ -24,14 +25,44 @@ def getArticles():
         cursor.execute(sql, bookID)
         result = cursor.fetchall()
         print(result)
-        return jsonify({"articles": result}), 400
+        return jsonify({"message": "Successfully!!", "articles": result}), 400
+
+
+def isArticleExisit(articleID):
+    with app.db.cursor() as cursor:
+        sql = 'SELECT ID FROM article WHERE ID = %s'
+        cursor.execute(sql, articleID)
+        result = cursor.fetchall()
+        return len(result) != 0
+
+
+@bp.route('', methods=['POST'])
+@login_required
+def putArticles(): 
+    bookID = request.form['bookID']
+    context = request.form['context']
+    chapter = request.form.get('chapter')
+    page = request.form.get('page')
+
+    with app.db.cursor() as cursor:
+        sql = '''
+        INSERT INTO article VALUES
+        (0, %s, %s, %s, %s, %s, %s)
+        '''
+        cursor.execute(sql, (current_user.id, bookID, context, 
+            datetime.date.today(), chapter, page))
+
+        app.db.commit()
+        return jsonify({"message": "Succcessfully registered"}), 400
 
 
 @bp.route('', methods=['DELETE'])
 @login_required
 def deleteArticle():
     articleID = request.args['articleID']
-    # 存在確認
+    if(not isArticleExisit(articleID)):
+        return jsonify({"message": "The article is not found"}), 404
+
     with app.db.cursor() as cursor:
         sql = '''
         DELETE article, book_marks, user_likes FROM article
@@ -54,28 +85,46 @@ def deleteArticle():
 @bp.route('', methods=['PUT'])
 @login_required
 def updateArticle():
-    articleID = request.args['articleID']
-    context = request.args['context']
+    articleID = request.form['articleID']
+
+    if(not isArticleExisit(articleID)):
+        return jsonify({"message": "The article is not found"}), 404
+
+    with app.db.cursor() as cursor:
+        param = dict(filter(lambda i: i[1] != None and i[0] != 'articleID', request.form.items()))
+
+        sets = map(lambda x: x + ' = %s', param.keys())
+        sql = 'UPDATE article set '
+        sql += ','.join(sets)
+        sql += 'WHERE ID = %s'
+
+        cursor.execute(sql, list(param.values()) + [articleID])
+        app.db.commit()
+        
+    return jsonify({"message": "Successfully update"}), 200
 
 
 @bp.route('/like', methods=['POST'])
 @login_required
 def registerLike():
-    articleID = request.args['articleID']
+    articleID = request.form['articleID']
     userID = current_user.id
+    if(not isArticleExisit(articleID)):
+        return jsonify({"message": "The article is not found"}), 404
+
     with app.db.cursor() as cursor:
         # 登録済みかチェック
         sql = '''
         SELECT COUNT(ID) FROM user_likes
-        WHERE articleID = %s AND userID = %s
+        WHERE userID = %s AND articleID = %s
         '''
         cursor.execute(sql, (userID, articleID))
-        if(cursor.fetchall() != 0):
+        if(cursor.fetchone()['COUNT(ID)'] != 0):
             return jsonify({"message": "Already Liked"}), 409
 
         # 登録
         sql = 'INSERT INTO user_likes VALUES(0, %s, %s)'
-        cursor.execute(sql, (0, userID, articleID))
+        cursor.execute(sql, (userID, articleID))
         app.db.commit()
         return jsonify({"message": "Successfully Registerd"}), 201
 
@@ -83,20 +132,23 @@ def registerLike():
 @bp.route('/bookmark', methods=['POST'])
 @login_required
 def registerBookmark():
-    articleID = request.args['ID']
+    articleID = request.form['articleID']
     userID = current_user.id
+    if(not isArticleExisit(articleID)):
+        return jsonify({"message": "The article is not found"}), 404
+
     with app.db.cursor() as cursor:
         # 登録済みかチェック
         sql = '''
         SELECT COUNT(ID) FROM book_marks 
-        WHERE articleID = %s AND userID = %s
+        WHERE userID = %s AND articleID = %s
         '''
         cursor.execute(sql, (userID, articleID))
-        if(cursor.fetchall() != 0):
+        if(cursor.fetchone()['COUNT(ID)'] != 0):
             return jsonify({"message": "Already Marked"}), 409
 
         # 登録
         sql = 'INSERT INTO book_marks VALUES(0, %s, %s)'
-        cursor.execute(sql, (0, userID, articleID))
+        cursor.execute(sql, (userID, articleID))
         app.db.commit()
         return jsonify({"message": "Successfully Registerd"}), 201
--- test/article_test.sh
+++ test/article_test.sh
@@ -5,7 +5,25 @@ curl -X GET -c cookie.txt http://localhost:8000/login/ | grep csrfmiddlewaretoke
 curl -i -X POST -b cookie.txt -c cookie.txt -d 'mailAddress=fuga@test.hoge.jp&password=password&csrfmiddlewaretoken=XXXXX' http://localhost:8000/login
 
 curl -X GET -b cookie.txt http://localhost:8000/article?bookID=1
+
 curl -X DELETE -b cookie.txt http://localhost:8000/article?articleID=1
 curl -X GET -b cookie.txt http://localhost:8000/article?bookID=1
 curl -X DELETE -b cookie.txt http://localhost:8000/article?articleID=100
+
+curl -X POST -b cookie.txt -d 'bookID=1&context=test&chapter=10&page=100' http://localhost:8000/article
+curl -X POST -b cookie.txt -d 'bookID=1&context=test&chapter=1' http://localhost:8000/article
+curl -X GET -b cookie.txt http://localhost:8000/article?bookID=1
+
+curl -X PUT -b cookie.txt -d 'articleID=7&context=hogehoge&chapter=1' http://localhost:8000/article
+curl -X PUT -b cookie.txt -d 'articleID=8&context=fugafuga&' http://localhost:8000/article
+curl -X GET -b cookie.txt http://localhost:8000/article?bookID=1
+
+# Like
+curl -X POST -b cookie.txt -d 'articleID=7' http://localhost:8000/article/like
+curl -X POST -b cookie.txt -d 'articleID=7' http://localhost:8000/article/like
+
+# Bookmark
+curl -X POST -b cookie.txt -d 'articleID=7' http://localhost:8000/article/bookmark
+curl -X POST -b cookie.txt -d 'articleID=7' http://localhost:8000/article/bookmark
+
 set +x
