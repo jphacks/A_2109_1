cb98d2cece0ae3b5f9f3966d274620cda79fdd31
tikisi <faohfh@gmail.com>
2021-10-31 00:06:51 +0900

    fix

--- test/article_test.sh
+++ test/article_test.sh
@@ -4,7 +4,8 @@ set -x
 curl -X GET -c cookie.txt http://localhost:8000/login/ | grep csrfmiddlewaretoken
 curl -i -X POST -b cookie.txt -c cookie.txt -d 'mail=fuga@test.hoge.jp&password=password&csrfmiddlewaretoken=XXXXX' http://localhost:8000/login
 
-curl -X GET -b cookie.txt http://localhost:8000/article?bookID=1
+curl -X GET -b cookie.txt http://localhost:8000/article?bookID=5
+exit
 curl -X GET -b cookie.txt http://localhost:8000/article?bookID=2
 
 curl -X DELETE -b cookie.txt http://localhost:8000/article?articleID=1
